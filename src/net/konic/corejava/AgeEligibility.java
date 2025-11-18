dae751cad1bc22e7e746488e9a0172210f1da830
syedhaneef9999-art <syedhaneef9999@gmail.com>
2025-11-18 18:43:21 +0530

    afe

--- src/net/konic/corejava/AgeEligibility.java
+++ src/net/konic/corejava/AgeEligibility.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 public class AgeEligibility {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        //logic
 
         System.out.print("Enter your category (OC / BC): ");
         String category = sc.next().toUpperCase();
