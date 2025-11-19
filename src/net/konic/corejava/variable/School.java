b0dd923aecf844156d2a0cba932032748a6d9495
syedhaneef9999-art <syedhaneef9999@gmail.com>
2025-11-19 11:40:46 +0530

    SCHOOL

---
+++ src/net/konic/corejava/variable/School.java
@@ -0,0 +1,22 @@
+package net.konic.corejava.variable;
+
+public class School {
+
+	public static void main(String[] args) {
+		String schoolName = "Green Valley High School";
+        int numberOfStudents = 1200;
+        float averageFee = 1500.75f;
+        double yearlyBudget = 25000000.55;
+        char schoolGrade = 'A';
+        boolean isPrivate = true;
+
+        System.out.println("School Name: " + schoolName);
+        System.out.println("Number of Students: " + numberOfStudents);
+        System.out.println("Average Fee: " + averageFee);
+        System.out.println("Yearly Budget: " + yearlyBudget);
+        System.out.println("School Grade: " + schoolGrade);
+        System.out.println("Is Private School: " + isPrivate);
+
+	}
+
+}
