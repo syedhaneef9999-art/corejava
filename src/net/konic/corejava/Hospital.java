9eba78819b089ac2d33476a2101ef0446ae53482
syedhaneef9999-art <syedhaneef9999@gmail.com>
2025-11-19 11:12:53 +0530

    hospital

---
+++ src/net/konic/corejava/Hospital.java
@@ -0,0 +1,20 @@
+package net.konic.corejava;
+
+public class Hospital {
+
+	public static void main(String[] args) {
+	String hospitalName="ragu";
+	int numberOfDoctors = 25;
+	float consultationFee = 299.50f;
+    double yearlyRevenue = 4500000.75;
+    char hospitalGrade = 'A';
+    boolean isOpen = true;
+    System.out.println("Hospital Name: " + hospitalName);
+    System.out.println("Number of Doctors: " + numberOfDoctors);
+    System.out.println("Consultation Fee: " + consultationFee);
+    System.out.println("Yearly Revenue: " + yearlyRevenue);
+    System.out.println("Hospital Grade: " + hospitalGrade);
+    System.out.println("Is Open: " + isOpen);
+	}
+
+}
