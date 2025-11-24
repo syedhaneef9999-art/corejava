ea224c5be0f3447f80b0125f832afadaca8b8e12
syedhaneef9999-art <syedhaneef9999@gmail.com>
2025-11-24 16:37:27 +0530

    oddpush

---
+++ src/net/konic/corejava/switchstatement/OddNumbersSwitch.java
@@ -0,0 +1,36 @@
+package net.konic.corejava.switchstatement;
+import java.util.Scanner;
+
+public class OddNumbersSwitch {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter limit: ");
+        int limit = sc.nextInt();   // user input
+
+        for (int i = 1; i <= limit; i++) {
+
+            if (i % 2 == 0) {
+                continue;  // skip even numbers
+            }
+
+            switch (i) {
+                case 1:
+                    System.out.println("1 → ONE");
+                    break;
+                case 3:
+                    System.out.println("3 → THREE");
+                    break;
+                case 5:
+                    System.out.println("5 → FIVE");
+                    break;
+                default:
+                    System.out.println(i + " → ODD NUMBER");
+            }
+        }
+        sc.close();
+    }
+}
+
+
