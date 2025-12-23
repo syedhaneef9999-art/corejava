190213c4b76022099b5d1950979397fe17932ed1
syedhaneef9999-art <244437103+syedhaneef9999-art@users.noreply.github.com>
2025-12-23 12:01:55 +0530

    collections

---
+++ src/net/konic/corejava/collections/Arraylist.java
@@ -0,0 +1,15 @@
+package net.konic.corejava.collections;
+
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+    }
+}
