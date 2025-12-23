abff6fa6fccbb1faabeb0e7396f87b1f580282ce
syedhaneef9999-art <syedhaneef9999@gmail.com>
2025-12-23 11:30:33 +0530

    hww

---
+++ src/net/konic/corejava/arrays/TwoD.java
@@ -0,0 +1,16 @@
+package net.konic.corejava.arrays;
+
+public class TwoD {
+	 public static void main(String[] args) {
+	        int[][] a = {{1,2},{3,4}};
+	        for(int i=0;i<a.length;i++){
+	            for(int j=0;j<a[i].length;j++){
+	                System.out.print(a[i][j]+" ");
+	            }
+	            System.out.println();
+	        }
+	    }
+
+
+
+}
